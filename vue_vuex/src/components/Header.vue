ae999bd815d7cc14a2115614d4260da11212308a
loop <1477824757@qq.com>
2023-07-06 21:33:59 +0800

    使用vuex

---
+++ vue_vuex/src/components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{ count }}</h1>
+    <button @click="vuex_test">vuex测试</button>
+</template>
+
+<script>
+    export default{
+        computed:{
+            count(){
+                return this.$store.state.count
+            }
+        },
+        methods:{
+            vuex_test(){
+                this.$store.commit('increment')
+                console.log(this.$store.state.count)
+            }
+        }
+    }
+</script>
