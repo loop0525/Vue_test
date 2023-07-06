80ad2d768dd89b1ee8f632eb045a7dc7a42c6428
loop <1477824757@qq.com>
2023-07-06 09:29:54 +0800

    使用element-plus

---
+++ vue_element_plus/src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>{{ word }}</h1>
+        <el-button type="success">Success</el-button>
+    </div>
+</template>
+
+<script scoped>
+    export default{
+        data(){
+            return{
+                word:"hello vue"
+            }
+        }
+    }
+</script>
